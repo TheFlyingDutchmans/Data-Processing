8469c1edf9a750d650fe90e5f9f146886552b084
HolyMelonSeed <sikora2012alex@gmail.com>
2022-06-27 12:05:51 +0200

    Added xml support

--- dv/dv.py
+++ dv/dv.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import folium
+import xml.etree.ElementTree as ET
 
 json_map = folium.Map(
     location=[34.825830, 18.359418],
@@ -10,6 +11,13 @@ json_map = folium.Map(
     width='100%',
     height='100%'
 )
+title_json = '''
+<h3 align="center" style="font-size:16px">JSON map</h3>
+'''
+
+title_xml = '''
+<h3 align="center" style="font-size:16px">XML map</h3>
+'''
 
 xml_map = folium.Map(
     location=[34.825830, 18.359418],
@@ -20,7 +28,7 @@ xml_map = folium.Map(
     height='100%'
 )
 
-url = "http://127.0.0.1:1234/api/getSpoofData"
+url = "http://127.0.0.1:5000/apiv3/getSpoofData"
 
 headers_json = {
     'Content-Type': 'application/json'
@@ -30,15 +38,16 @@ headers_xml = {
     'Content-Type': 'application/xml'
 }
 
+
 def getAllSpoofData():
     get_spoof_data_payload_xml = "<root>\r\n    " \
-                                    "<request></request>\r\n    " \
-                                    "<user>\r\n        " \
-                                        "<apiKey>admin</apiKey>\r\n    " \
-                                    "</user>\r\n" \
+                                 "<user>\r\n        " \
+                                 "<apiKey>admin</apiKey>\r\n    " \
+                                 "</user>\r\n" \
                                  "</root>\r\n "
     response_xml = requests.request("GET", url, headers=headers_xml, data=get_spoof_data_payload_xml)
-    response_info_xml = response_xml
+    # get xml from the response and
+    response_info_xml = ET.fromstring(response_xml.text)
 
     get_spoof_data_payload_json = json.dumps({
         "request": {
@@ -49,23 +58,25 @@ def getAllSpoofData():
     })
     response_json = requests.request("GET", url, headers=headers_json, data=get_spoof_data_payload_json)
     response_info_json = json.loads(response_json.text)
-    print(response_info_json)
     return response_info_json, response_info_xml
 
 
 def getSingleSpoofData(id):
     get_spoof_data_payload_xml = "<root>\r\n    " \
-                                    "<request>" + str(id) + "</request>\r\n    " \
-                                    "<user>\r\n        " \
-                                        "<apiKey>admin</apiKey>\r\n    " \
-                                    "</user>\r\n" \
-                                 "</root>\r\n "
+                                 "<request>\r\n        " \
+                                 "<spoofID>" + str(id) + "</spoofID>\r\n    " \
+                                                         "</request>\r\n" \
+                                                         "<user>\r\n        " \
+                                                         "<apiKey>admin</apiKey>\r\n    " \
+                                                         "</user>\r\n" \
+                                                         "</root>\r\n "
     response_xml = requests.request("GET", url, headers=headers_xml, data=get_spoof_data_payload_xml)
-    response_info_xml = response_xml
+    response_info_xml = ET.fromstring(response_xml.text)
+    print(response_xml.text)
 
     get_spoof_data_payload_json = json.dumps({
         "request": {
-            "spoofID": str(id)
+            "spoofID": id
         },
         "user": {
             "apiKey": "admin"
@@ -76,31 +87,53 @@ def getSingleSpoofData(id):
 
     return response_info_json, response_info_xml
 
+
 def displaySingleShip(id):
     response_info_json, response_info_xml = getSingleSpoofData(id)
-    addMarker(response_info_json["spoofData"], json_map)
-    #addMarker(response_info_xml, xml_map)
+    addMarkerJson(response_info_json["spoofData"])
+
+    lat = response_info_xml.find('spoofData').find('latitude').text
+    long = response_info_xml.find('spoofData').find('longitude').text
+    time = response_info_xml.find('spoofData').find('currentTime').text
+    print(lat, long, time)
+    addMarkerXml(lat, long, time)
 
 
 def displayAllShips():
     response_info_json, response_info_xml = getAllSpoofData()
+    # iterate over spoofData in JSON file and add it to a map
     for spoof in response_info_json["spoofData"]:
-        addMarker(spoof, json_map)
-    #addMarker(response_info_xml, xml_map)
+        addMarkerJson(spoof)
+
+    # iterate over spoofData in XML file and add it to a map
+    for spoof in response_info_xml.find('spoofData'):
+        lat = spoof.find('latitude').text
+        long = spoof.find('longitude').text
+        time = spoof.find('currentTime').text
+        addMarkerXml(lat, long, time)
 
 
-
-def addMarker(spoof, m):
-    print(spoof)
+def addMarkerJson(spoof):
     lat = spoof["latitude"]
     long = spoof["longitude"]
     time = spoof["currentTime"]
     folium.Marker(
         location=[lat, long],
         popup=str(time),
-    ).add_to(m)
+    ).add_to(json_map)
+
+
+def addMarkerXml(lat, long, time):
+    folium.Marker(
+        location=[lat, long],
+        popup=str(time),
+    ).add_to(xml_map)
+
 
 displayAllShips()
-displaySingleShip(10)
+
+json_map.get_root().html.add_child(folium.Element(title_json))
 json_map.save('json_map.html')
+
+xml_map.get_root().html.add_child(folium.Element(title_xml))
 xml_map.save('xml_map.html')
